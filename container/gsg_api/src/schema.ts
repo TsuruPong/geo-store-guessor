e7a7f853f6d5b353c745f257d987dadbf08d394d
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2024-01-03 16:47:17 +0900

    add: typedef and resolver

--- container/gsg_api/src/schema.ts
+++ container/gsg_api/src/schema.ts
@@ -1,17 +1,29 @@
-import { makeExecutableSchema } from '@graphql-tools/schema'
- 
-const typeDefinitions = /* GraphQL */ `
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const typeDefinitions = `
   type Query {
-    hello: String!
+    stores: [Store!]!
   }
-`
+
+  type Store {
+    id: ID!
+    name: String!
+    address: String!
+    lat: Float!
+    lng: Float!
+  }
+`;
  
 const resolvers = {
   Query: {
-    hello: () => 'Hello World!'
+    stores: () => {
+      return prisma.store.findMany();
+    }
   }
 }
- 
+
 export const schema = makeExecutableSchema({
   resolvers: [resolvers],
   typeDefs: [typeDefinitions]
